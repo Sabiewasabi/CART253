ae15606fa08530f6f35e6affb9984e0f62470e4c
Sabiewasabi <sabrinarath26@gmail.com>
2026-09-15 15:57:02 -0400

    Got the land to show up
    Fiddled with the proportions and position of the rectangle to be a the bottom with the perfect height and thickness.

--- Landscape challenge/js/script.js
+++ Landscape challenge/js/script.js
@@ -5,10 +5,27 @@ function setup (){
 function draw(){
     background('#CF2913');
     
+    //Draw land
+    fill('#020122');
+    rect(0,500,800,200)
+
     //Draw moon 
+    push();
+
+    fill('#FFF3D1');
     ellipse(320,280,150);
   
+    fill('#020122')
+    triangle(200,350,158, 100, 20,300);
+     fill('#020122')
+    triangle(100,500,70, 100, 20,300);
+
+    pop();
+
+
 }
+
+
 
 
     
