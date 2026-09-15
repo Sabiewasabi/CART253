585ee6b918a27c5be991b1a09f64f8cf0456390e
Sabiewasabi <sabrinarath26@gmail.com>
2026-09-14 20:37:37 -0400

    Copy pasted the code onto VS code
    Coded my prototype on p5.js first to test out how it will look then pasted it here.

---
+++ Prototypes/Instructions/Instructions-prototype-2/Emulation gamestation.js
@@ -0,0 +1,47 @@
+//Title:Emulation gamestation
+
+// Author:Sabrina Rath
+
+// This prototype intends to be able to play a game (maybe pong or a maze game) through the screen of a Gameboy.
+
+function setup() {
+  createCanvas(600,600);
+
+  background('#EE92C2');
+
+  //Draw gameboy body
+  rectMode (CENTER);
+  fill ('#46237A');
+  rect (300, 300, 300, 400);
+  
+  //Draw gameboy screen
+  fill (0);
+  rect (300,220,250,200);
+  fill ('#C4E0F9');
+  rect (300,220,200,150);
+
+  
+  //Text
+  fill (0);
+  //Setting variable for text size
+  let s = 'PLAY A GAME';
+  textSize (20)
+  text(s, 330, 300, 200, 160);
+  
+
+  // Buttons
+  fill(0);
+  ellipse (400,370,35);
+  ellipse (350,400,35);
+  
+  // D pad button
+  //top
+  rect(210,400,40,55);
+  //left
+  rect(180,420,40);
+  //right
+  rect(240,420,40);
+  //bottom
+  rect(210,440,40,55);
+  
+}
